d24b0721ca7d06b4d993312cd33ecc0730790f1f
laluferdianyusuf <lferdianyusuf@gmail.com>
2023-11-08 15:23:09 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import LoginUser from "./pages/LoginUser";
 import Dashboard from "./pages/Dashboard";
 
 const root = document.getElementById("root");
@@ -12,6 +13,7 @@ render(
   <Router>
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/login/:number" element={<LoginUser />} />
       <Route path="/register" element={<Register />} />
       <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
